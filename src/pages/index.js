dbacec34db4e8b3c033a12df66e1c33b68b0d6e3
Chris Daynes <chrisdaynes@Chriss-MacBook-Pro.local>
2021-03-13 20:21:08 +1300

    added module css

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,15 @@
 import React from "react"
+import { Link } from 'gatsby'
+import Header from '../components/header'
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div style={{ color: "red", fontSize: '36px' }}>
+      <Link to='/contact/'>Contact</Link>
+      <Header headerText="this is the header Text"/>
+      <p>What a world!</p>
+      <img src="https://source.unsplash.com/random/400x200" alt="" />
+
+    </div>
+  )
 }
